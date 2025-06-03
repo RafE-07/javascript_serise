855ff37c1151b00f30827233bf4a615fb882776a
RafE-07 <mahmudrezarafi@gmail.com>
2025-06-03 09:15:00 +0000

    updated datatypes.js file

---
+++ BASIC_01/datatype.js
@@ -0,0 +1,20 @@
+"use strict";//treat as js code as newer version
+//alert(3-3)//not allowed as we are using node.js not browser
+console.log(3+3);
+
+let name="reza"
+let age= 18
+let isloggedin = false
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null=> stadalone value .. store null 
+//undifined=>not defined value
+//symbol=> unique
+
+
+//object
+
+
